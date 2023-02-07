a2d197b6a3cf994c94354b74333f80001af8b96a
AlejandroXRD <106454456+AleSGXRD@users.noreply.github.com>
2023-02-06 19:44:24 -0500

    server

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import express from "express";
+
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
 import indexRouter from "./routes/index.js";
+import {PORT} from "./config.js"
 
 const app = express();
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set("views", join(_dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); 
 app.use(indexRouter);
 
-console.log(_dirname);
 app.use(express.static(join(_dirname, "public")));
 
-app.listen(process.env.PORT || 3000);
-console.log("Server on port", process.env.PORT || 3000);
+app.listen(PORT);
+console.log("Server on port", PORT);
